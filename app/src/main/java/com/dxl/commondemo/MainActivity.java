a092de366b2d399125f491120eaa3f82a983d5e4
duxiaolong <du_xiaolong@hotmail.com>
2020-06-03 17:46:13 +0800

    初次提交

---
+++ app/src/main/java/com/dxl/commondemo/MainActivity.java
@@ -0,0 +1,22 @@
+package com.dxl.commondemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * desc   :
+ *
+ * @author : dxl
+ * @version : 1.0
+ * e-mail : du_xiaolong@hotmail.com
+ * @date : 2020/6/3 5:41 PM
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
